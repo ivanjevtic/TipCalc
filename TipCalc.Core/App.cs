f41557b20eebf44595bd6682ea4547ff86d2dcfd
ivanjevtic <ivanj@exe-net.net>
2013-11-20 23:56:09 +0100

    Core added

---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalc.Core.Classes;
+using TipCalc.Core.Interface;
+using TipCalc.Core.Models;
+
+namespace TipCalc.Core
+{
+    public class App : MvxApplication
+    {
+        public App()
+        {
+            Mvx.RegisterType<ICalculation, Calculation>();
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<TipCalcModel>());
+        }
+    }
+}
