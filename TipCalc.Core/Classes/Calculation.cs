1ce6dbfdead7894e8de1412047e6ab78bec95ed1
ivanjevtic <ivanj@exe-net.net>
2013-11-20 23:56:54 +0100

    rest

---
+++ TipCalc.Core/Classes/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalc.Core.Interface;
+
+namespace TipCalc.Core.Classes
+{
+    public class Calculation : ICalculation
+    {
+        public double TipAmount(double subTotal, int generosity)
+        {
+            return subTotal * (double)generosity/(double)100.0;
+        }
+    }
+}
---
+++ TipCalc.Core/Models/TipCalcModel.cs
@@ -0,0 +1,16 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalc.Core.Models
+{
+    public class TipCalcModel : MvxViewModel
+    {
+        public TipCalcModel()
+        { 
+        }   
+    }
+}
